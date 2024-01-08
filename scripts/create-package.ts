2fa3f8d5081ee09f5a62467bb3b3d8d6269d813a
André Felipe Schulle <andrefelipeschulle@icloud.com>
2024-01-08 18:39:36 -0300

    add github actions

---
+++ scripts/create-package.ts
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+(async () => {
+  const sourcePath = path.resolve('./');
+  const fromPath = path.resolve('./dist');
+
+  // copy package.json to build out
+  const originalPackage = fs.readFileSync(
+    path.join(sourcePath, 'package.json'),
+  );
+  const packageParsed = JSON.parse(originalPackage.toString());
+
+  delete packageParsed.scripts;
+  delete packageParsed.devDependencies;
+
+  const newPackage = JSON.stringify(packageParsed, null, 2);
+
+  fs.writeFileSync(path.join(fromPath, 'package.json'), newPackage);
+  console.info('package.json copied to build out');
+})();
