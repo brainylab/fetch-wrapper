9cb856ed46b0c54095c036632e447931a107f678
André Felipe Schulle <andrefelipeschulle@icloud.com>
2024-08-01 23:52:16 -0300

    feat(remove-object-nullish): removendo objetos que são undefined ou null dos parametros

--- src/utils/object-to-url-params.ts
+++ src/utils/object-to-url-params.ts
@@ -1,9 +1,24 @@
+function cleanObject(
+	obj: Record<string, string | number | string[] | number[] | undefined | null>,
+): Record<string, string | number | string[] | number[]> {
+	const cleanedObj: Record<string, string | number | string[] | number[]> = {};
+	Object.keys(obj).forEach((key) => {
+		const value = obj[key];
+		if (value !== undefined && value !== null) {
+			cleanedObj[key] = value;
+		}
+	});
+
+	return cleanedObj;
+}
+
 export function objectToUrlParams(
 	obj: Record<string, string | number | string[] | number[]>,
 ): string {
+	const cleanedObj = cleanObject(obj);
 	const params = new URLSearchParams();
 
-	Object.keys(obj).map((key) => {
+	Object.keys(cleanedObj).map((key) => {
 		if (Object.prototype.hasOwnProperty.call(obj, key)) {
 			if (Array.isArray(obj[key])) {
 				const arr = obj[key] as string[] | number[];
